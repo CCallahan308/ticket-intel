ce7d9a5bd87d187708e89bef6075202c302d15ed
ccallahan308 <calla_chris@proton.me>
2026-04-05 09:14:47 -0500

    clean up readme, remove debug files

--- test_ann2.py
+++
@@ -1,8 +0,0 @@
-class Meta(type):
-    def __new__(mcs, name, bases, namespace):
-        print("namespace keys:", list(namespace.keys()))
-        return super().__new__(mcs, name, bases, namespace)
-
-class A(metaclass=Meta):
-    a: int
-    b: str = "hi"
